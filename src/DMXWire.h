dc11ae3a1e36f4d42aa4938bd54284b48ba3bee9
Chris <c.wuepping@gmail.com>
2022-12-31 14:25:26 +0100

    MasterTX2

--- src/DMXWire.h
+++ src/DMXWire.h
@@ -9,39 +9,41 @@
 #define DMXWIRE_H_
 #include <Arduino.h>
 #include <Wire.h>
-//#include <ESP32TimerInterrupt.h>
+#include <esp_timer.h>
+#include <Ticker.h>
 
-#define TIMER_INTERVAL_MS        35
-#define DMXW_PACKETSIZE 32
-#define DMXW_PACKETS 512 / DMXW_PACKETSIZE
+#define DMXWIRE_INTERVAL_MS        1000
+
+#define DMXWIRE_BYTES_PER_PACKET 32
+#define DMXWIRE_PACKETS 512 / DMXWIRE_BYTES_PER_PACKET
+
+#define DMXWIRE_NOTBUSY -1
 
 class DMXWire {
 public:
-	DMXWire(uint8_t timerNo);
-	void setClock(uint32_t frequency);	//set I2C clock
-	void beginSender(uint8_t scl,uint8_t sda, uint8_t slaveaddress );
-	void beginReceiver(uint8_t scl,uint8_t sda );
-	void runSender();
-	void runReceiver();
-	void write(uint16_t channel, uint8_t byte);
-	uint8_t read(uint16_t channel);
+	DMXWire();
+	static void setClock(uint32_t frequency);	//set I2C clock
+	static void beginMasterTX(uint8_t scl,uint8_t sda, uint8_t slaveaddress );
+	// static void beginMasterRX(uint8_t scl,uint8_t sda );
+	static void write(uint16_t channel, uint8_t byte);
+	static uint8_t read(uint16_t channel);
 
 	virtual ~DMXWire();
 private:
-	unsigned long timer = 0;
-	uint8_t buffer[DMXW_PACKETS][DMXW_PACKETSIZE];
-	uint8_t packetNo = 0;
-	uint8_t slaveAddress = 1;	//slave's address
+	static uint8_t packets[DMXWIRE_BYTES_PER_PACKET][DMXWIRE_PACKETS];
+	static uint8_t packetNo;
+	static uint8_t slaveAddress;	//slave's address
 
-	void requestData();
-	void setPacket();
-	void sendPacket();
+	static void requestData();
+	static void setPacket();
+	static void sendPacket();
 
-//	/** IR stuff **/
-//	bool IRAM_ATTR IRhandlerTX(void * timerNo);
-//	ESP32Timer *ITtimer;
+	/** IR stuff **/
+	static Ticker IRtimer;	// siehe: https://github.com/espressif/arduino-esp32/issues/3465
+	static void IRhandler();
+	static int packetBusy;
 
 
-};
+}; extern DMXWire Dmxwire;
 
 #endif /* LIBRARIES_DMXWIRE_DMXWIRE_H_ */
